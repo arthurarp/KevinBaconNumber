c2a3ff3cd153e3a23d0aede6dc7a0b44e15f83dc
Arthur <arthurarp@hotmail.com>
2022-05-08 01:22:25 -0300

    add graph visualizing
    Signed-off-by: Arthur <arthurarp@hotmail.com>

--- kbnumber/src/pages/Home/index.js
+++ kbnumber/src/pages/Home/index.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import Graph from "react-graph-vis";
+import options from "../../jsons/options.json";
 import {
   Container,
   Header,
@@ -12,6 +14,26 @@ import {
 export default function Home() {
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
   const [userInputed, setUserInputed] = useState("");
+  const graph = {
+    nodes: [
+      { id: 1, label: "Node 1", title: "node 1 tootip text" },
+      { id: 2, label: "Node 2", title: "node 2 tootip text" },
+      { id: 3, label: "Node 3", title: "node 3 tootip text" },
+      { id: 4, label: "Node 4", title: "node 4 tootip text" },
+      { id: 5, label: "Node 5", title: "node 5 tootip text" }
+    ],
+    edges: [
+      { from: 1, to: 2 },
+      { from: 1, to: 3 },
+      { from: 2, to: 4 },
+      { from: 2, to: 5 }
+    ]
+  };
+  const events = {
+    select: function(event) {
+      var { nodes, edges } = event;
+    }
+  };
 
   useEffect(() => {
     function handleResize() {
@@ -52,7 +74,11 @@ export default function Home() {
           
         </SideMenu>
         <GraphContainer>
-
+          <Graph 
+            graph={graph}
+            options={options} 
+            events={events} 
+          />
         </GraphContainer>
       </Body>
     </Container>
