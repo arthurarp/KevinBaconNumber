83627d00699ba39e2aba97b3a8eb0391e3ec3d3d
Arthur <arthurarp@hotmail.com>
2022-05-08 01:16:43 -0300

    add user input
    Signed-off-by: Arthur <arthurarp@hotmail.com>

--- kbnumber/src/pages/Home/index.js
+++ kbnumber/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Container,
   Header,
@@ -6,13 +6,55 @@ import {
   SideMenu,
   GraphContainer,
   Text,
+  Input,
 } from "./styles";
 
 export default function Home() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [userInputed, setUserInputed] = useState("");
 
-    return (
-      <Container>
-        
-      </Container>
-    );
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Text>
+          {windowDimensions.width} + {windowDimensions.height}
+        </Text>
+      </Header>
+      <Body>
+        <SideMenu>
+          <Input 
+            value={userInputed}
+            required
+            onChange={(input) => {
+              setUserInputed(input.target.value);
+              console.log(userInputed)
+            }}
+            placeholder="git user"
+          
+          />
+          
+        </SideMenu>
+        <GraphContainer>
+
+        </GraphContainer>
+      </Body>
+    </Container>
+  );
 }
--- kbnumber/src/pages/Home/styles.js
+++ kbnumber/src/pages/Home/styles.js
@@ -1,10 +1,68 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: 1000px;
-  background: #000;
+  height: 750px;
+  /* background: #000; */
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+`;
+export const Header = styled.div`
+  height: 10%;
+  width: 100%;
+  flex-direction: column;
+  vertical-align: middle;
+  background: #000;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+`;
+export const Body = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const SideMenu = styled.div`
+  height: 100%;
+  width: 15%;
+  background: #ccc;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  border-right-width: 1px;
+  border-right-color: #000;
+`;
+
+export const GraphContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  /* background: #f00; */
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const Text = styled.p`
+  margin-top: 15px;
+  font-size: 20px;
+  color: #ff0000;
+  text-align: center;
+`;
+
+export const Input = styled.input`
+  background: rgba(0, 0, 0, 0.1);
+  border: 1;
+  border-radius: 4px;
+  height: 35px;
+  padding: 0 15px;
+  color: #000;
+  margin: 0 0 10px;
+  &::placeholder {
+    color: rgba(120, 120, 120, 0.7);
+  }
 `;
